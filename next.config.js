de77b04d2a74bb4d4cf514d967227974ee477fc2
syz10086 <s888666ok@163.com>
2025-01-12 23:05:41 +0800

    fix:update

---
+++ next.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    async redirects() {
+        return [
+            {
+                source: '/game', // 用户访问的路径
+                destination: '/game', // 要重定向到的路径
+                permanent: true, // 是否为永久重定向 (301)
+            },
+            {
+                source: '/leaderboard', // 用户访问的路径
+                destination: '/leaderboard', // 要重定向到的路径
+                permanent: true, // 是否为永久重定向 (301)
+            },
+            {
+                source: '/tutorial', // 用户访问的路径
+                destination: '/tutorial', // 要重定向到的路径
+                permanent: true, // 是否为永久重定向 (301)
+            },
+            {
+                source: '/faq', // 用户访问的路径
+                destination: '/faq', // 要重定向到的路径
+                permanent: true, // 是否为永久重定向 (301)
+            },
+        ];
+    },
+};
